9f57f9a820404950af41f4b892e7dcaba0a0398f
danielbontii <bontiidaniel97@gmail.com>
2024-02-07 17:25:41 +0000

    chore: add starter files

---
+++ src/main/java/com/danielbontii/tpms/TaskAndProfileManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.danielbontii.tpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAndProfileManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAndProfileManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielbontii/tpms/TaskAndProfileManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielbontii.tpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAndProfileManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
