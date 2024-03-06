0e6df4cbd458c921a0ed85076275a0745153818a
danielbontii <bontiidaniel97@gmail.com>
2024-03-06 10:55:00 +0000

    perf: configure actuator for monitoring

---
+++ src/main/java/com/danielbontii/tpms/configs/ActuatorConfig.java
@@ -0,0 +1,16 @@
+package com.danielbontii.tpms.configs;
+
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorConfig {
+
+    @Bean
+    public HttpExchangeRepository httpTraceRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
+
+}
